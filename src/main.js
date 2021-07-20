7eb8d2663b8bea7105b367a791712cbd1898a4d7
irisdev06 <ivgb_@outlook.com>
2021-07-20 14:40:06 -0500

    Primera historia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/lol/lol.js';
+for (const i in data.data) {
+  document.getElementById(
+    "root"
+  ).innerHTML += `<div class="champClass"><img src="${data.data[i].img}">
+    <p>${data.data[i].name}</p><i>${data.data[i].title}</i></div>`;
+}
 
 console.log(example, data);
