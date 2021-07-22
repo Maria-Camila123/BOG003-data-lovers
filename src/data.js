98dae155a838e365af012cc0e5e1f96450f614e7
irisdev06 <ivgb_@outlook.com>
2021-07-22 08:45:55 -0500

    TagFilter

--- src/data.js
+++ src/data.js
@@ -1,9 +1,28 @@
-// estas funciones son de ejemplo
+import data from "./data/lol/lol.js";
 
-export const example = () => {
-  return 'example';
+const listChampions = data.data;
+
+export const filterChampions = (role) => {
+  let filterArray = [];
+  let j = 0;
+  if (role != "All") {
+    for (const i in listChampions) {
+      listChampions[i].tags.forEach((roleChamp) => {
+        if (roleChamp == role) {
+          filterArray[j] = listChampions[i].id;
+          j++;
+        }
+      });
+    }
+  } else {
+    for (const i in listChampions) {
+      filterArray[j] = listChampions[i].id;
+      j++;
+    }
+  }
+  return filterArray;
 };
 
 export const anotherExample = () => {
-  return 'OMG';
+  return "OMG";
 };
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-import { example } from './data.js';
-import data from './data/lol/lol.js';
+import { filterChampions } from "./data.js";
+import data from "./data/lol/lol.js";
+const listChampions = data.data;
 
+document.getElementById("roles").addEventListener("change", function () {
+  let role = document.getElementById("roles").value;
+  let listChamps = filterChampions(role);
+  //console.log(listChamps);
+  document.getElementById("root").innerHTML = ``;
+  listChamps.forEach((champ) => {
+    document.getElementById(
+      "root"
+    ).innerHTML += `<div class="champClass"><img src="${listChampions[champ].splash}">
+    <p>${listChampions[champ].name}</p><i>${listChampions[champ].title}</i></div>`;
+  });
+});
 
-for (const i in data.data) {
+for (const i in listChampions) {
   document.getElementById(
     "root"
-  ).innerHTML += `<div class="champClass"><img src="${data.data[i].splash}">
-    <p>${data.data[i].name}</p><i>${data.data[i].title}</i></div>`;
+  ).innerHTML += `<div class="champClass"><img src="${listChampions[i].splash}">
+    <p>${listChampions[i].name}</p><i>${listChampions[i].title}</i></div>`;
 }
 
-console.log(example, data);
+//console.log(example, data);
