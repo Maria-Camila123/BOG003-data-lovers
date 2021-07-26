d677808c4598481fdfa0cddd4f0803bfc18bf7df
Maria-Camila123 <na01alvarez@gmail.com>
2021-07-26 08:34:35 -0500

    Intro de cada rol

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { filterChampions } from "./data.js";
 import data from "./data/lol/lol.js";
+
 const listChampions = data.data;
 
-document.getElementById("roles").addEventListener("change", function () {
+document.getElementById("roles").addEventListener("change", () => {
   let role = document.getElementById("roles").value;
+  console.log(role);
   let listChamps = filterChampions(role);
-  //console.log(listChamps);
+  showRoles(role);
+  //hiddenRoles(All);
   document.getElementById("root").innerHTML = ``;
   listChamps.forEach((champ) => {
     document.getElementById(
@@ -15,6 +18,8 @@ document.getElementById("roles").addEventListener("change", function () {
   });
 });
 
+
+
 for (const i in listChampions) {
   document.getElementById(
     "root"
@@ -23,3 +28,122 @@ for (const i in listChampions) {
 }
 
 //console.log(example, data);
+
+const hiddenRoles = () => {
+
+  let assassinText = document.getElementById("role-assassin");
+  let fighterText = document.getElementById("role-fighter");
+  let mageText = document.getElementById("role-mage");
+  let tankText = document.getElementById("role-tank");
+  let marksmanText = document.getElementById("role-marksman");
+  let supportText = document.getElementById("role-support");
+
+  assassinText.style.display = "none";
+  fighterText.style.display = "none";
+  mageText.style.display = "none";
+  tankText.style.display = "none";
+  marksmanText.style.display = "none";
+  supportText.style.display = "none";
+  
+}
+
+hiddenRoles();
+//let championRole = document.getElementById("roles");
+
+
+
+const showRoles = (role) => {
+   //let championRoleDiv = document.getElementById("champion-role");
+   //role es igual a el valor de los section con el id "roles"  
+   
+  
+  let assassinText = document.getElementById("role-assassin");
+  let fighterText = document.getElementById("role-fighter");
+  let mageText = document.getElementById("role-mage");
+  let tankText = document.getElementById("role-tank");
+  let marksmanText = document.getElementById("role-marksman");
+  let supportText = document.getElementById("role-support");
+
+
+  //assassinText.style.display = "block";
+  if (role == "Assassin"){
+    //championRoleDiv.innerHTML=""
+    assassinText.style.display = "block";
+    fighterText.style.display = "none";
+    mageText.style.display = "none";
+    tankText.style.display = "none";
+    marksmanText.style.display = "none";
+    supportText.style.display = "none";
+
+  }else if (role == "Fighter"){
+    //championRoleDiv.innerHTML=""
+    assassinText.style.display = "none";
+    fighterText.style.display = "block";
+    mageText.style.display = "none";
+    tankText.style.display = "none";
+    marksmanText.style.display = "none";
+    supportText.style.display = "none";
+  
+  }else if (role == "Mage"){
+    //championRoleDiv.innerHTML=""
+    assassinText.style.display = "none";
+    fighterText.style.display = "none";
+    mageText.style.display = "block";
+    tankText.style.display = "none";
+    marksmanText.style.display = "none";
+    supportText.style.display = "none";
+
+  }else if (role == "Tank"){
+    //championRoleDiv.innerHTML=""
+    assassinText.style.display = "none";
+    fighterText.style.display = "none";
+    mageText.style.display = "none";
+    tankText.style.display = "block";
+    marksmanText.style.display = "none";
+    supportText.style.display = "none";
+
+  }else if (role == "Marksman"){
+    //championRoleDiv.innerHTML=""
+    assassinText.style.display = "none";
+    fighterText.style.display = "none";
+    mageText.style.display = "none";
+    tankText.style.display = "none";
+    marksmanText.style.display = "block";
+    supportText.style.display = "none";
+  
+  }else if (role == "Support"){
+    //championRoleDiv.innerHTML=""
+    assassinText.style.display = "none";
+    fighterText.style.display = "none";
+    mageText.style.display = "none";
+    tankText.style.display = "none";
+    marksmanText.style.display = "none";
+    supportText.style.display = "block";
+  
+  }else if (role == "All"){
+    //championRoleDiv.innerHTML=""
+    assassinText.style.display = "none";
+    fighterText.style.display = "none";
+    mageText.style.display = "none";
+    tankText.style.display = "none";
+    marksmanText.style.display = "none";
+    supportText.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+/*
+selectAssassin.addEventListener("click", productos);
+activities.addEventListener("change", function() {
+  if(activities.value == "addNew")
+  {
+      addActivityItem();
+  }
+});*/
+
+
